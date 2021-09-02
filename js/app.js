924b4ab785d80e0e65e04fbab76598d2da165e4f
Springhawk27 <sajjad0mahmud@gmail.com>
2021-09-02 20:38:24 +0600

    done

--- js/app.js
+++ js/app.js
@@ -4,23 +4,20 @@ const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
-    // // clear input field
+    // clear input field
     searchField.value = '';
 
-    if (searchText == '') {
-        displayEmptyMessage();
-    }
 
     // load data
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(response => response.json())
-        .then(data => displaySearchResult(data.docs))
+        .then(data => displaySearchResult(data.docs, data.numFound))
 
-}
+};
 
-
-const displaySearchResult = docs => {
+// display search result and search count
+const displaySearchResult = (docs, searchLength) => {
 
     const searchResult = document.getElementById('search-result');
     const searchCount = document.getElementById('search-count');
@@ -29,6 +26,7 @@ const displaySearchResult = docs => {
     searchCount.textContent = '';
 
 
+    // checking whether the search result is 0 or not
     if (docs.length == 0) {
         // console.log('No Result Found');
 
@@ -46,17 +44,11 @@ const displaySearchResult = docs => {
         <h1>No Results Found</h1>
     `;
         noResult.appendChild(h1);
-    }
+    };
 
-
+    // looping on array docs by using forEach
     docs.forEach(doc => {
-        // console.log(docs.length);
-
-        // console.log(docs);
-
         const div = document.createElement('div');
-
-        // div.classList.add('');
 
         div.innerHTML = `
        
@@ -69,50 +61,28 @@ const displaySearchResult = docs => {
             <p><span class="font-semibold">Publisher:</span>  ${doc.publisher}</p>
             <p><span class="font-semibold">First Published:</span> ${doc.first_publish_year}</p>
         </div>
-        
         `;
         searchResult.appendChild(div);
-        // toggleSearchResult('none')
         toggleNoResult('none');
 
 
 
     });
 
+    // displaying search count
     const div = document.createElement('div');
 
     div.innerHTML = `
-        <h1>Search Count: ${docs.length} </h1>
+        <h1>Search Count: ${searchLength} </h1>
 
     </div>
     `;
     searchCount.appendChild(div);
 
-}
+};
 
-displaying message if input is empty
-const displayEmptyMessage = () => {
-    const emptySearch = document.getElementById('empty-search');
-    toggleSearchResult('block');
-    toggleNoResult('none');
-
-
-    emptySearch.textContent = '';
-    const h1 = document.createElement('h1');
-
-    h1.innerHTML = `
-        <h1>Please enter some text!!!</h1>
-    `;
-    emptySearch.appendChild(h1);
-
-}
-
-
-const toggleSearchResult = displayStyle => {
-    document.getElementById('empty-search').style.display = displayStyle;
-}
-
+// No Results Found mesaage toggler function
 const toggleNoResult = displayStyle => {
     document.getElementById('no-result').style.display = displayStyle;
-}
+};
 
